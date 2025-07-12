12d53af6189599957be1a55ad4f9ae80deacdb46
Prasanna0401 <prasannavenkatesh2002@gmail.com>
2025-07-12 17:57:30 +0530

    BE Link

--- web-client/src/lib/axios.js
+++ web-client/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "/api/v1" : "/api/v1",
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://realtimechatapplicationbe.onrender.com/api/v1",
   withCredentials: true,
 });
